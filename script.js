4486c0c6dbd3c74644d77367c31facc44d7f60b4
siskiakia <siskiapplg@gmail.com>
2023-12-05 03:48:03 +0700

    menambahkan style css

---
+++ script.js
@@ -0,0 +1,8 @@
+//navigation
+function toggleMenu() {
+    const toggleMenu = document.querySelector(".toggleMenu");
+    const navigation = document.querySelector(".navigation");
+    toggleMenu.classList.toggle("active");
+    toggleMenu.classList.toggle("active");
+}
+//end navigation
